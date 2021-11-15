95ab89e52b39f99d8c723ba3bce0822d370b5b14
Nick <nsagias@gmail.com>
2021-11-15 01:13:44 -0500

    create count in controllers for products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,3 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
+  def show
+    @category = Category.count()
+    @products = Product.count()
+  end
+
 end
