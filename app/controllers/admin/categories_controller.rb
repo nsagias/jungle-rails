f3a2881d8e486f41a3266159f336d4b789af6a54
Nick <nsagias@gmail.com>
2021-11-15 16:29:31 -0500

    Add new and create to controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,5 +3,19 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Categories created!'
+    else
+      render :new
+    end
+  end
 
 end
