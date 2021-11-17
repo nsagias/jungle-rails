19cf4ea4c452c4b337ff370b48ffc32b4c120e68
Nick <nsagias@gmail.com>
2021-11-16 22:35:57 -0500

    add spec test and name test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    category = Category.create(name: "test")
+    product = Product.new(name: "bob", quantity: 1, price: 10, category: category)
+    expect(product).to be_valid
+  end
+  it "is not valid without a name" do
+    category = Category.create(name: "test")
+    product = Product.new(name: nil , quantity: 1, price: 10, category: category)
+    expect(product).to_not be_valid
+    # product.save
+    # byebug
+    # byebug message
+    # (byebug) product.errors.full_messages
+    # ["Name can't be blank"]
+    # (byebug) 
+  end
+
+end
