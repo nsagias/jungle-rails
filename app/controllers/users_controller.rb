a292a7d6652cc0e02cf5d1c02d02da1e9c9b012e
Nick <nsagias@gmail.com>
2021-11-16 23:50:10 -0500

    Add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 # app/controllers/users_controller.rb
-
 class UsersController < ApplicationController
 
   def new
   end
 
   def create
-  end   
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:fname, :lname, :email, :password, :password_confirmation)
+  end
+  
 end
