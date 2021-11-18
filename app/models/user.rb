cfbbfcb8dbbf737c089c58cd1f27a31c94a8749c
Nick <nsagias@gmail.com>
2021-11-17 20:34:50 -0500

    Wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :fname, presence: true
   validates :lname, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,49 +2,57 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
 
-  it "is valid with valid attributes" do
-    user = User.new(fname: "bob", lname: "uncle", email:"test@test.com", password: "password", password_confirmation:"password")
-    expect(user).to be_valid
-  end
-  it "is not valid without fname" do
-    user = User.new(fname: nil, lname: "uncle", email:"test@test.com", password: "password", password_confirmation:"password")
-    expect(user).to_not be_valid
-  end
+    it "is valid with valid attributes" do
+      user = User.new(fname: "bob", lname: "uncle", email:"test@test.com", password: "password", password_confirmation:"password")
+      expect(user).to be_valid
+    end
+    it "is not valid without fname" do
+      user = User.new(fname: nil, lname: "uncle", email:"test@test.com", password: "password", password_confirmation:"password")
+      expect(user).to_not be_valid
+    end
+    
+    it "is not valid without lname" do
+      user = User.new(fname: "bob", lname: nil, email:"test@test.com", password: "password", password_confirmation:"password")
+      expect(user).to_not be_valid
+    end
+    it "is not valid without email" do
+      user = User.new(fname: "bob", lname: "uncle", email: nil, password: "password", password_confirmation:"password")
+      expect(user).to_not be_valid
+    end
+    it "is not valid without password" do
+      user = User.new(fname: "bob", lname: "uncle", email: "test@test.com", password: nil, password_confirmation:"password")
+      expect(user).to_not be_valid
+    end
+    it "is not valid without password_confirmation" do
+      user = User.new(fname: "bob", lname: "uncle", email: "test@test.com", password: "password", password_confirmation: nil)
+      expect(user).to_not be_valid
+    end
+    it "is not valid if password and password confirmation do not match" do
+      user = User.new(fname: "bob", lname: "uncle", email: "test@test.com", password: "password", password_confirmation: "1134")
+      expect(user).to_not be_valid
+    end
+    it "is not valid if email is already in database" do
+      User.create(fname: "bob", lname: "uncle", email:"test@test.COM", password: "password", password_confirmation:"password")
+      user = User.new(fname: "bob", lname: "uncle", email: "TEST@TEST.com", password: "password", password_confirmation: "1134")
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid if password length is less than 3 chars" do
+      user = User.new(fname: "bob", lname: "uncle", email:"test@test.com", password: "pa", password_confirmation:"password")
+      expect(user).to_not be_valid
+    end
+    it "is not valid if password_confirmation length is less than 3 chars" do
+      user = User.new(fname: "bob", lname: "uncle", email:"test@test.com", password: "password", password_confirmation:"pa")
+      expect(user).to_not be_valid
+    end
   
-  it "is not valid without lname" do
-    user = User.new(fname: "bob", lname: nil, email:"test@test.com", password: "password", password_confirmation:"password")
-    expect(user).to_not be_valid
-  end
-  it "is not valid without email" do
-    user = User.new(fname: "bob", lname: "uncle", email: nil, password: "password", password_confirmation:"password")
-    expect(user).to_not be_valid
-  end
-  it "is not valid without password" do
-    user = User.new(fname: "bob", lname: "uncle", email: "test@test.com", password: nil, password_confirmation:"password")
-    expect(user).to_not be_valid
-  end
-  it "is not valid without password_confirmation" do
-    user = User.new(fname: "bob", lname: "uncle", email: "test@test.com", password: "password", password_confirmation: nil)
-    expect(user).to_not be_valid
-  end
-  it "is not valid if password and password confirmation do not match" do
-    user = User.new(fname: "bob", lname: "uncle", email: "test@test.com", password: "password", password_confirmation: "1134")
-    expect(user).to_not be_valid
-  end
-  it "is not valid if email is already in database" do
-    User.create(fname: "bob", lname: "uncle", email:"test@test.COM", password: "password", password_confirmation:"password")
-    user = User.new(fname: "bob", lname: "uncle", email: "TEST@TEST.com", password: "password", password_confirmation: "1134")
-    expect(user).to_not be_valid
   end
 
-  it "is not valid if password length is less than 3 chars" do
-    user = User.new(fname: "bob", lname: "uncle", email:"test@test.com", password: "pa", password_confirmation:"password")
-    expect(user).to_not be_valid
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
   end
-  it "is not valid if password_confirmation length is less than 3 chars" do
-    user = User.new(fname: "bob", lname: "uncle", email:"test@test.com", password: "password", password_confirmation:"pa")
-    expect(user).to_not be_valid
-  end
-  
+
+
 end
