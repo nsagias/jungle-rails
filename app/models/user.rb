25dbf5cca9b92cc4fc6bcf066bfb5db736c5553b
Nick <nsagias@gmail.com>
2021-11-17 20:06:39 -0500

    Add user_spec and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true
+  validates :fname, presence: true
+  validates :lname, presence: true
+ 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with valid attributes" do
+    user = User.new(fname: "bob", lname: "uncle", email:"test@test.com", password: "password", password_confirmation:"password")
+    expect(user).to be_valid
+  end
 end
