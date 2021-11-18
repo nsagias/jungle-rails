063f87795faa0289e5df00e1c7b5124a199e1eee
Nick <nsagias@gmail.com>
2021-11-18 01:49:38 -0500

    Add click_on add

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,13 +1,38 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to Add to cart", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
 
   scenario "They see all products" do
     # ACT
     visit root_path
+    click_on('Add')
 
-    # DEBUG / VERIFY
-    save_screenshot
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+
+    # DEBUG
+    # save_screenshot
+
+    # VERIFY
+    # "My Cart (1)"
+    # expect(page).to have_css 'article.product', count: 10
   end
-
 end
